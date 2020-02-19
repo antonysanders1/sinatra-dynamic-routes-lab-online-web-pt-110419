278feb807d30e95dde581f5efc217ac52ba33e80
Antony Sanders <antony.devon92@gmail.com>
2020-02-19 18:54:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,9 +9,9 @@ class App < Sinatra::Base
   end
   
   get '/square/:number' do
-    @number = params[:number]
+    @number = params[:number].to_i
     #binding.pry
-    "#{params[:number].to_i**2}"
+    @number**2
   end 
   
   get '/say/:number/:phrase' do 
